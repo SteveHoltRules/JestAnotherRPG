2ee1e995e62a97fe1d2adf8054a305ad88b8cc78
Matt Luna <mat.c.luna@gmail.com>
2021-09-03 10:37:19 -0500

    Mock test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,9 @@
-const { TestWatcher } = require('jest');
+const Potion = require('../lib/Potion');
 const Player = require('../lib/Player');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion);
 
 test('creates a player object',() => {
   const player = new Player('Dave');
@@ -8,4 +12,7 @@ test('creates a player object',() => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+
 });
+
